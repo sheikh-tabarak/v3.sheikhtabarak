c8f1521f4894f652ecc4dfc5340f283edf5d5ce7
sheikh-tabarak <tabarakyaseen46@gmail.com>
2026-07-23 19:13:48 +0500

    feat: add custom _document.js with external time-machine script integration

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://www.sheikhtabarak.me/time-machine.js"
+          data-current-version="v3"
+          defer
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
